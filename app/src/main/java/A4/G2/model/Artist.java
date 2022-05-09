a6681d78538b16724d501ccb292ffe2c54990269
yuchendong <68779035+yuchendong13@users.noreply.github.com>
2022-05-09 20:23:06 +1200

    testLinkGenerator - add code to fix compile errors

--- app/src/main/java/A4/G2/model/Artist.java
+++ app/src/main/java/A4/G2/model/Artist.java
@@ -3,4 +3,8 @@ package A4.G2.model;
 public class Artist extends User{
     private String name;
     private Art[] arts;
+
+    public String getArtistId() {
+        return "";
+    }
 }
---
+++ app/src/main/java/A4/G2/service/LinkGenerator.java
@@ -0,0 +1,7 @@
+package A4.G2.service;
+
+public class LinkGenerator implements ILinkGenerator {
+	public String generateLink(String customerName) {
+		return "";
+	}
+}
