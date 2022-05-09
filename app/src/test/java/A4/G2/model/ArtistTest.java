52927e82d04ee4db5aa444a981bebe6b2aeff99b
Bruce Zeng <bruce.zeng@xtra.co.nz>
2022-05-09 22:20:59 +1200

    refactor

---
+++ app/src/test/java/A4/G2/model/ArtistTest.java
@@ -0,0 +1,32 @@
+package A4.G2.model;
+
+import A4.G2.service.LinkGenerator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class ArtistTest {
+	Artist artist;
+
+	@BeforeAll
+	public void setUp() {
+		 artist = new Artist();
+	}
+
+	@Test
+	public void testArtistPageLinkGeneration() {
+		LinkGenerator artistLink = new LinkGenerator();
+		String link = artistLink.generateLink(artist.getArtistId());
+		assertEquals("https://www.g2gallery.com/"+artist.getArtistId(), link);
+	}
+
+	@Test
+	public void testCheckArtistIdNotEmpty(){
+		assertNotSame("", artist.getArtistId());
+	}
+
+}
--- app/src/test/java/A4/G2/service/LinkGeneratorTest.java
+++ app/src/test/java/A4/G2/service/LinkGeneratorTest.java
@@ -2,18 +2,18 @@ package A4.G2.service;
 
 import A4.G2.model.Artist;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.Mockito.*;
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class testLinkGenerator {
+public class LinkGeneratorTest {
 
 	@Test
-	public void should_return_artist_page_link() {
+	public void testArtistPageLinkGenerator() {
 		String uuid = UUID.randomUUID().toString();
-		LinkGenerator artistLink = new LinkGenerator();;
+		LinkGenerator artistLink = new LinkGenerator();
 		Artist artist = Mockito.mock(Artist.class);
 		Mockito.doReturn(uuid).when(artist).getArtistId();
 		String link = artistLink.generateLink(artist.getArtistId());
