b2af2aea34979800e96fceabf6a2e4467eec88f1
Bruce Zeng <bruce.zeng@xtra.co.nz>
2022-05-10 19:42:28 +1200

    Add code to make test case pass

--- app/src/main/java/A4/G2/model/Gallery.java
+++ app/src/main/java/A4/G2/model/Gallery.java
@@ -2,8 +2,24 @@ package A4.G2.model;
 
 import A4.G2.model.artwork.Art;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Gallery {
-    private String termsAndConditions;
-    private Art[] arts;
+    private List<Art> arts = new ArrayList<>();
     private Art[] onSales;
+
+    public Gallery() {
+        String termsAndConditions = "";
+    }
+
+    public void addArt(Art art) {
+        arts.add(art);
+    }
+    public List<Art> getArtList() {
+        return arts;
+    }
+
+
+
 }
