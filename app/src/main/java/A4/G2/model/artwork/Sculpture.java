79bf951677da60f817036229191752db48369b29
yuchendong <68779035+yuchendong13@users.noreply.github.com>
2022-05-10 17:16:41 +1200

    testGetSculptureArtist - add code to make test case pass

--- app/src/main/java/A4/G2/model/artwork/Sculpture.java
+++ app/src/main/java/A4/G2/model/artwork/Sculpture.java
@@ -5,9 +5,14 @@ import A4.G2.model.users.Artist;
 import java.awt.*;
 
 public class Sculpture extends Art {
-
+    private Artist artist;
 
     public Sculpture(Artist artist, String title, String description, Image photo) {
         super(artist, title, description, photo);
+        this.artist = artist;
+    }
+
+    public Artist getSculptureArtist() {
+        return this.artist;
     }
 }
