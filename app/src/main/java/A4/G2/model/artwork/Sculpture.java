883ccaf2d5c671431d69962bce8956ccfd925a2a
yuchendong <68779035+yuchendong13@users.noreply.github.com>
2022-05-10 17:18:07 +1200

    testGetSculptureTitle - add code to make failing test case pass

--- app/src/main/java/A4/G2/model/artwork/Sculpture.java
+++ app/src/main/java/A4/G2/model/artwork/Sculpture.java
@@ -6,13 +6,19 @@ import java.awt.*;
 
 public class Sculpture extends Art {
     private Artist artist;
+    private String title;
 
     public Sculpture(Artist artist, String title, String description, Image photo) {
         super(artist, title, description, photo);
         this.artist = artist;
+        this.title = title;
     }
 
     public Artist getSculptureArtist() {
         return this.artist;
+    }
+
+    public String getSculptureTitle() {
+        return this.title;
     }
 }
