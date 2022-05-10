d043337a35f735fbb7a3a1b49e1d75597f9e2bcd
yuchendong <68779035+yuchendong13@users.noreply.github.com>
2022-05-10 16:50:46 +1200

    testGetPaintingTitle - add code to make test case pass

--- app/src/main/java/A4/G2/model/artwork/Painting.java
+++ app/src/main/java/A4/G2/model/artwork/Painting.java
@@ -6,13 +6,19 @@ import java.awt.*;
 
 public class Painting extends Art {
     private Artist artist;
+    private String title;
 
     public Painting(Artist artist, String title, String description, Image photo) {
         super(artist, title, description, photo);
         this.artist = artist;
+        this.title = title;
     }
 
     public Artist getPaintingArtist() {
         return this.artist;
+    }
+
+    public String getPaintingTitle() {
+        return this.title;
     }
 }
