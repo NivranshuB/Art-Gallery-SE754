9bca224dec7487889440d6c50f934fe0f36e2429
yuchendong <68779035+yuchendong13@users.noreply.github.com>
2022-05-10 17:09:10 +1200

    testGetPrintArtist - add code to make test case pass

--- app/src/main/java/A4/G2/model/artwork/Print.java
+++ app/src/main/java/A4/G2/model/artwork/Print.java
@@ -5,8 +5,14 @@ import A4.G2.model.users.Artist;
 import java.awt.*;
 
 public class Print extends Art {
+    private Artist artist;
 
     public Print(Artist artist, String title, String description, Image photo) {
         super(artist, title, description, photo);
+        this.artist = artist;
+    }
+
+    public Artist getPrintArtist() {
+        return this.artist;
     }
 }
