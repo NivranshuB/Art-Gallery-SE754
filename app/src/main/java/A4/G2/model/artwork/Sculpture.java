2c3bf34b1abc11dab8da07dde9fe16ff291a95c2
yuchendong <68779035+yuchendong13@users.noreply.github.com>
2022-05-10 17:19:14 +1200

    testGetSculptureDescription - add code to make test case pass

--- app/src/main/java/A4/G2/model/artwork/Sculpture.java
+++ app/src/main/java/A4/G2/model/artwork/Sculpture.java
@@ -7,11 +7,13 @@ import java.awt.*;
 public class Sculpture extends Art {
     private Artist artist;
     private String title;
+    private String description;
 
     public Sculpture(Artist artist, String title, String description, Image photo) {
         super(artist, title, description, photo);
         this.artist = artist;
         this.title = title;
+        this.description = description;
     }
 
     public Artist getSculptureArtist() {
@@ -20,5 +22,9 @@ public class Sculpture extends Art {
 
     public String getSculptureTitle() {
         return this.title;
+    }
+
+    public String getSculptureDescription() {
+        return this.description;
     }
 }
