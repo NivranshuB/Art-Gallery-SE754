6812b7219a1c67ed30c18bc64d8b10bd3a642c4c
yuchendong <68779035+yuchendong13@users.noreply.github.com>
2022-05-10 17:07:17 +1200

    testGetPrintArtist - add failing test case

---
+++ app/src/test/java/A4/G2/model/artwork/testGetPrintDetails.java
@@ -0,0 +1,32 @@
+package A4.G2.model.artwork;
+
+import A4.G2.model.users.Artist;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mockito;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class testGetPrintDetails {
+	Artist artist;
+	Image image;
+	Print print;
+	@BeforeAll
+	public void setUp() throws IOException {
+		artist = Mockito.mock(Artist.class);
+		image = ImageIO.read(new File("src/test/java/A4/G2/model/artwork/testImage.png"));
+		print = Mockito.spy(new Print(artist, "Art Title", "Art Description", image));
+	}
+
+	@Test
+	public void testGetPrintArtist() {
+		assertEquals(artist, print.getPrintArtist());
+	}
+}
