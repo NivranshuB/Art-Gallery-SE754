22d24a95b3366e71e1c263aa5a79002d4e4ca258
Bruce Zeng <bruce.zeng@xtra.co.nz>
2022-05-11 16:20:08 +1200

    Refactor

--- app/src/test/java/A4/G2/model/GalleryTest.java
+++ app/src/test/java/A4/G2/model/GalleryTest.java
@@ -1,18 +1,28 @@
 package A4.G2.model;
 
 import A4.G2.model.artwork.Art;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class GalleryTest {
+
+    Gallery gallery;
+    Art art;
+    @BeforeAll
+    public void setUp() {
+        gallery = Mockito.spy(new Gallery());
+        art = Mockito.mock(Art.class);
+    }
 
     @Test
     public void testAddingArtToGalleryArtList() {
-        Gallery gallery = new Gallery();
-        Art art = Mockito.mock(Art.class);
         gallery.addArt(art);
         assertEquals(gallery.getArtList().size(), 1);
     }
+
 }
