4f504767928cbd99286ff742d84e75df44c4faba
yuchendong <68779035+yuchendong13@users.noreply.github.com>
2022-05-12 17:52:57 +1200

    testArtPageLinkGenerator - add failing test case

--- app/src/test/java/A4/G2/service/LinkGeneratorTest.java
+++ app/src/test/java/A4/G2/service/LinkGeneratorTest.java
@@ -1,10 +1,14 @@
 package A4.G2.service;
 
+import A4.G2.model.artwork.Painting;
 import A4.G2.model.users.Artist;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,6 +23,14 @@ public class LinkGeneratorTest {
 		Mockito.doReturn(uuid).when(artist).getArtistId();
 		String link = artistLink.generateLink(artist.getArtistId());
 		assertEquals("https://www.g2gallery.com/"+uuid, link);
+	}
+
+	@Test
+	public void testArtPageLinkGenerator() {
+		String uuid = UUID.randomUUID().toString();
+		LinkGenerator paintingLinkGenerator = new LinkGenerator();
+		String link = paintingLinkGenerator.generateArtLink(uuid);
+		assertEquals("https://www.g2gallery.com/art/"+uuid, link);
 	}
 
 }
