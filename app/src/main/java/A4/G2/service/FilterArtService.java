d225c8acb73600998b8e28a033bd9153c5eead75
NivranshuB <nbos433@aucklanduni.ac.nz>
2022-05-12 16:10:58 +1200

    Add code to make test case pass

--- app/src/main/java/A4/G2/service/FilterArtService.java
+++ app/src/main/java/A4/G2/service/FilterArtService.java
@@ -1,5 +1,6 @@
 package A4.G2.service;
 
+import A4.G2.model.sale.Auction;
 import A4.G2.model.sale.Sale;
 
 import java.util.ArrayList;
@@ -8,6 +9,13 @@ import java.util.List;
 public class FilterArtService implements IFilterArtService{
     @Override
     public List<Sale> getAuctionItems(List<Sale> saleList) {
-        return new ArrayList<>();
+        List<Sale> auctionItemsList = new ArrayList<>();
+
+        for (Sale sale : saleList) {
+            if (sale instanceof Auction) {
+                auctionItemsList.add(sale);
+            }
+        }
+        return auctionItemsList;
     }
 }
