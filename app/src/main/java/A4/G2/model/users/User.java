1e223659570ff4d245c38f240373a17da01de24f
ljey973 <ljey973@aucklanduni.ac.nz>
2022-05-16 01:56:32 +1200

    Add failing test case for creation of a valid payment.

--- app/src/main/java/A4/G2/model/users/User.java
+++ app/src/main/java/A4/G2/model/users/User.java
@@ -1,12 +1,30 @@
 package A4.G2.model.users;
 
+import java.util.UUID;
+
 import A4.G2.model.Payment;
 
 public class User {
+    private String id;
     private String userName;
     private String password;
     private String email;
     private String phone;
     private String address;
     private Payment payment;
+
+
+public User(String userName, String password, String email, String phone, String address) {
+    this.userName = userName;
+    this.password = password;
+    this.email = email;
+    this.phone = phone;
+    this.address = address;
+    this.id = this.id = UUID.randomUUID().toString();
+}
+
+	public void modifyPayment(Payment payment) {
+	    this.payment = payment;
+	}
+
 }
---
+++ app/src/test/java/A4/G2/model/users/PaymentTest.java
@@ -0,0 +1,67 @@
+package A4.G2.model.users;
+
+import A4.G2.model.Payment;
+import A4.G2.model.artwork.Painting;
+import A4.G2.model.sale.Auction;
+import A4.G2.model.users.Artist;
+import A4.G2.model.users.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mockito;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class PaymentTest {
+	Artist artist;
+	User user;
+	Image image;
+	Painting painting;
+	Auction auction;
+	int saleId;
+	double price;
+	double reservePrice;
+	int timeRemaining;
+	Payment payment;
+
+	@BeforeAll
+	public void setUp() throws IOException {
+		artist = Mockito.mock(Artist.class);
+		image = ImageIO.read(new File("src/test/java/A4/G2/model/artwork/testImage.png"));
+		painting = Mockito.spy(new Painting(artist, "Art Title", "Art Description", image, "2x1m"));
+
+		saleId = 2;
+		price = 50.00; // starting price
+		reservePrice = 200.00;
+		timeRemaining = 7; // days
+		auction = Mockito.spy(new Auction(saleId, price, painting, reservePrice, timeRemaining));
+
+		user = Mockito.spy(new User("Luxman", "Luxman", "luxman.gmail.com", "0222222222", "9 Narnia Land"));
+	}
+
+	@Test
+	public void testValidCreatePayment() {
+		Payment payment = new Payment(cardNumber,cardHolder,expiryDate,CVV);
+		assertEquals(payment.getCardNumber(),"5555555555554444");
+		assertEquals(payment.getCardHolder(),"Luxman");
+		assertEquals(payment.getExpiryDate(),"02/21");
+		assertEquals(payment.getExpiryDate(),"333");
+
+	}
+
+	private Payment getValidPayment() {
+		String cardNumber = "5555555555554444";
+		String cardHolder = "Luxman";
+		String expiryDate = "02/21";
+		String CVV = "333";
+		return new Payment("5555555555554444","Luxman","02/21","333");
+	}
+
+}
+
