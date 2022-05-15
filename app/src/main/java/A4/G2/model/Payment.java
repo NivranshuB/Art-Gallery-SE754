87b127521297c2cafe60d307dbbd414bb61fb9ed
ljey973 <ljey973@aucklanduni.ac.nz>
2022-05-16 02:31:28 +1200

    Add code so that test case passes.

--- app/src/main/java/A4/G2/model/Payment.java
+++ app/src/main/java/A4/G2/model/Payment.java
@@ -1,12 +1,21 @@
 package A4.G2.model;
 
+import A4.G2.service.payment.InvalidPaymentException;
+import A4.G2.service.payment.PaymentVerifier;
+
 public class Payment {
     private String cardNumber;
     private String cardHolder;
     private String expiryDate;
     private String CVV;
 
-    public Payment(String cardNumber,String cardHolder,String expiryDate,String CVV) {
+    public Payment(String cardNumber,String cardHolder,String expiryDate,String CVV){
+        try {
+            PaymentVerifier.verifyPayment(cardNumber,cardHolder,expiryDate,CVV);
+        }
+        catch (InvalidPaymentException ex) {
+        }
+
         this.cardNumber = cardNumber;
         this.cardHolder = cardHolder;
         this.expiryDate = expiryDate;
