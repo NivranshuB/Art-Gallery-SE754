21ac6658603b50c53f07fc394e340443147fac03
ljey973 <ljey973@aucklanduni.ac.nz>
2022-05-16 02:02:44 +1200

    Added code to pass testValidCreatePayment.

--- app/src/main/java/A4/G2/model/Payment.java
+++ app/src/main/java/A4/G2/model/Payment.java
@@ -5,4 +5,27 @@ public class Payment {
     private String cardHolder;
     private String expiryDate;
     private String CVV;
+
+    public Payment(String cardNumber,String cardHolder,String expiryDate,String CVV) {
+        this.cardNumber = cardNumber;
+        this.cardHolder = cardHolder;
+        this.expiryDate = expiryDate;
+        this.CVV = CVV;
+    }
+
+    public String getCardNumber() {
+        return this.cardNumber;
+    }
+
+    public String getCardHolder() {
+        return this.cardHolder;
+    }
+
+    public String getExpiryDate() {
+        return this.expiryDate;
+    }
+
+    public String getCVV() {
+        return this.CVV;
+    }
 }
