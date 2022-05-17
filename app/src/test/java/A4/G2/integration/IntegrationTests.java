06db4765a3cae9eb3e70fe30e0d4ffbae25becd0
Bruce Zeng <bruce.zeng@xtra.co.nz>
2022-05-17 14:19:39 +1200

    Add integration test - user with payment buy art from gallery

--- app/src/test/java/A4/G2/integration/IntegrationTests.java
+++ app/src/test/java/A4/G2/integration/IntegrationTests.java
@@ -1,9 +1,9 @@
 package A4.G2.integration;
 
 import A4.G2.model.Gallery;
+import A4.G2.model.Payment;
 import A4.G2.model.artwork.Painting;
 import A4.G2.model.sale.BuyNow;
-import A4.G2.model.sale.Sale;
 import A4.G2.model.users.Artist;
 import A4.G2.model.users.User;
 import org.junit.jupiter.api.Test;
@@ -35,6 +35,32 @@ public class IntegrationTests {
                 fail("Expected exception not thrown");
             } catch (Exception e) {
                 assert(e.getMessage().equals("User has no payment details."));
+            }
+
+        } else {
+            fail("Art not added correctly to gallery");
+        }
+    }
+
+    @Test
+    public void testBuyArtInGalleryWithPaymentDetails() throws IOException {
+        User user = new User("username", "password", "email.gmail", "0248195812", "123 address" );
+        Payment payment = new Payment("1234567890123456", "User User", "01/20", "123");
+        user.modifyPayment(payment);
+
+        Artist artist = new Artist();
+        Image image = ImageIO.read(new File("src/test/java/A4/G2/model/artwork/testImage.png"));
+        Painting painting = new Painting(artist, "title", "description", image, "2x1m");
+        BuyNow buyNow = new BuyNow(1, 15.99, painting);
+
+        Gallery gallery = new Gallery();
+        gallery.addArtForSale(buyNow);
+
+        if (gallery.getArtSalesList().get(0) instanceof BuyNow) {
+            try{
+                ((BuyNow) gallery.getArtSalesList().get(0)).buyArtPiece(user);
+            } catch (Exception e) {
+                fail("Expected exception not thrown");
             }
 
         } else {
