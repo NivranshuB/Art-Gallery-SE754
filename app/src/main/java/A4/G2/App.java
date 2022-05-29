f0b81f9167e949b7ad93a6b60738de5b1f20504b
Bruce Zeng <bruce.zeng@xtra.co.nz>
2022-05-29 17:21:16 +1200

    Setup jsp

--- app/src/main/java/A4/G2/App.java
+++ app/src/main/java/A4/G2/App.java
@@ -3,12 +3,14 @@
  */
 package A4.G2;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        SpringApplication.run(App.class, args);
     }
 }
