f6b1fbf3ce0fccce22e801ebc81c9c9543b872ad
Bruce Zeng <bruce.zeng@xtra.co.nz>
2022-06-04 18:59:13 +1200

    Implement Step definitions

---
+++ app/src/test/java/A4/G2/web/stepdefs/EditArtStepDefinitions.java
@@ -0,0 +1,47 @@
+package A4.G2.web.stepdefs;
+
+import A4.G2.web.pages.ArtDetailsPage;
+import A4.G2.web.pages.EditArtPage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class EditArtStepDefinitions {
+    private WebDriver driver;
+    private EditArtPage editArtPage;
+    private ArtDetailsPage artDetailsPage;
+
+    @Given("Driver set up for edit art page")
+    public void driverSetUpForEditArtPage(){
+        driver = Hooks.getDriver();
+        editArtPage = new EditArtPage(driver);
+        artDetailsPage = new ArtDetailsPage(driver);
+    }
+    @Given("I am on an edit art details page for {string}")
+    public void iAmOnAnEditArtDetailsPageFor(String string) {
+        driver.get("http://localhost:8080/edit-art?id="+string);
+    }
+
+
+    @When("I enter {string} as edit title field")
+    public void iEnterAsEditTitleField(String string) {
+        editArtPage.insertNewTitle(string);
+    }
+
+    @And("I click on the submit edit button")
+    public void iClickOnTheSubmitEditButton() {
+        editArtPage.clickSubmit();
+    }
+
+    @Then("I should see successfully edit message")
+    public void iShouldSeeSuccessfullyEditMessage() {
+        assertTrue(editArtPage.getMessage().contains("Art successfully edited"));
+    }
+
+
+}
--- app/src/test/resources/features/editArt.feature
+++ app/src/test/resources/features/editArt.feature
@@ -1,10 +1,10 @@
-Feature: ManageExistingArt
+Feature: EditArt
 
   Background:
     Given Driver set up for edit art page
 
   Scenario: Edit title of art
-    Given I am on an art-details page for "id"
+    Given I am on an edit art details page for "id"
     When I enter "New Title" as edit title field
     And I click on the submit edit button
     Then I should see successfully edit message
