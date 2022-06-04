58cdfd4c6401c0a4bc7d59620cd9f543c436bc85
NivranshuB <nbos433@aucklanduni.ac.nz>
2022-06-04 19:43:10 +1200

    Implement step definitions

--- app/src/test/java/A4/G2/web/stepdefs/SignupStepDefinitions.java
+++ app/src/test/java/A4/G2/web/stepdefs/SignupStepDefinitions.java
@@ -35,4 +35,37 @@ public class SignupStepDefinitions {
     public void i_should_see_the_signup_page() {
         assertTrue(driver.getCurrentUrl().contains("signup"));
     }
+
+    @Given("I visit the signup page")
+    public void i_visit_the_signup_page() {
+        driver.get("http://localhost:8080/signup");
+    }
+    @When("I enter {string} as new account user name field")
+    public void i_enter_as_new_account_user_name_field(String string) {
+        signupPage.insertUserName(string);
+    }
+    @When("I enter {string} as new account password field")
+    public void i_enter_as_new_account_password_field(String string) {
+        signupPage.insertPassword(string);
+    }
+    @When("I enter {string} as new account retyped password field")
+    public void i_enter_as_new_account_retyped_password_field(String string) {
+        signupPage.insertRetypedPassword(string);
+    }
+    @When("I tick {string} for terms and conditions")
+    public void i_tick_for_terms_and_conditions(String string) {
+        boolean termsAccepted = Boolean.valueOf(string);
+        if (termsAccepted) {
+            signupPage.clickConditions();
+        }
+    }
+    @When("I press the signup submit button")
+    public void i_press_the_signup_submit_button() {
+        signupPage.clickSignup();
+    }
+    @Then("I should see the invalid credentials message")
+    public void i_should_see_the_invalid_credentials_message() {
+        assertTrue(signupPage.getErrorMessage().contains("Invalid Credentials"));
+    }
+
 }
